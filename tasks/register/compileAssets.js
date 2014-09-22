e4f594e5acdc7bcc7581bc13ef58e24228182ea6
Alexey Vasilenko <lxvslnk@gmail.com>
2014-09-22 19:43:40 +0700

    change task order

--- tasks/register/compileAssets.js
+++ tasks/register/compileAssets.js
@@ -2,8 +2,8 @@ module.exports = function (grunt) {
 	grunt.registerTask('compileAssets', [
 		'clean:dev',
 		'html2js:dev',
-		'compass:dev',
 		'copy:dev',
+		'compass:dev',
 		'coffee:dev'
 	]);
 };
