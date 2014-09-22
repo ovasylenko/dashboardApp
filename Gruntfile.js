b056ea8829a5f6caaa78205edc48abdef84d9484
Alexey Vasilenko <lxvslnk@gmail.com>
2014-09-22 17:33:47 +0700

    fx grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,20 +13,7 @@
  */
 
 module.exports = function(grunt) {
-	//ToDo:Move
-	var depsPath = grunt.option('gdsrc') || 'node_modules/sails/node_modules';
-	grunt.loadTasks(depsPath + '/grunt-contrib-clean/tasks');
-	grunt.loadTasks(depsPath + '/grunt-contrib-copy/tasks');
-	grunt.loadTasks(depsPath + '/grunt-contrib-concat/tasks');
-	grunt.loadTasks(depsPath + '/grunt-sails-linker/tasks');
-	grunt.loadTasks(depsPath + '/grunt-contrib-jst/tasks');
-	grunt.loadTasks(depsPath + '/grunt-contrib-watch/tasks');
-	grunt.loadTasks(depsPath + '/grunt-contrib-uglify/tasks');
-	grunt.loadTasks(depsPath + '/grunt-contrib-cssmin/tasks');
-	grunt.loadTasks(depsPath + '/grunt-contrib-less/tasks');
-	grunt.loadTasks(depsPath + '/grunt-contrib-coffee/tasks');
-	grunt.loadTasks('node_modules/grunt-contrib-sass/tasks');
-	grunt.loadTasks('node_modules/grunt-contrib-compass/tasks');
+
 	// Load the include-all library in order to require all of our grunt
 	// configurations and task registrations dynamically.
 	var includeAll;
